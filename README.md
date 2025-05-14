99aaef569e429cbeedd5a53d367aa2d1f91a75f1
ETIENNE CHAZELLE <etienne.chazelle@nomachine.univ-eiffel.fr>
2025-05-14 08:43:18 +0200

    modification des fichiers distances

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cerp-rouen
-Projet pluridisciplinaire S2 ESIEE Paris E3 D&amp;AD
+Projet pluridisciplinaire S2 ESIEE Paris E3
--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 const API_KEY = "5b3ce3597851110001cf6248717a265e1c504f209faf31ce6733c995"
+const STORAGE_CERP = "600 Rue des Madeleines, 77100 Mareuil-lès-Meaux";
 
-async function searchCity(city, postalCode) {
+async function searchCoords(address, postalCode) {
     const response = await fetch(
-      `https://api-adresse.data.gouv.fr/search/?q=${city}&postcode=${postalCode}&limit=1`
+      `https://api-adresse.data.gouv.fr/search/?q=${address}&postcode=${postalCode}&limit=1`
     );
     const data = await response.json();
     return data.features[0].geometry.coordinates;
@@ -19,9 +20,17 @@ fs.readFile('data/livraison5.csv', 'utf8', async (err, data) => {
 
     const lines = data.split('\n');
     const pharmacies = [];
-    for (let i = 0; i < lines.length - 1; i++) {
+    pharmacies.push({
+        id: 0,
+        name: "CERP",
+        address: STORAGE_CERP,
+        postalCode: "77100",
+        city: "Mareuil-lès-Meaux",
+        coordinates: await searchCoords(STORAGE_CERP, "77100")
+    });
+    for (let i = 1; i < lines.length - 1; i++) {
         const [name, address, postalCode, city] = lines[i].split(',');
-        const coordinates = await searchCity(city, postalCode);
+        const coordinates = await searchCoords(address, postalCode);
         pharmacies.push({
             id: i,
             name,
@@ -35,21 +44,29 @@ fs.readFile('data/livraison5.csv', 'utf8', async (err, data) => {
     var distances = [];
 
     // Calculation of the distances between each pair of pharmacies
-    console.log(pharmacies.length);
     for (let i = 0; i < pharmacies.length; i++) {
-        for (let j = i + 1; j < pharmacies.length; j++) {
-            // Appel API
-            const response = await fetch(
-                `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${API_KEY}&start=${pharmacies[i].coordinates}&end=${pharmacies[j].coordinates}`
-            );
-            const data = await response.json();
-            console.log(data);
-            distances.push({
-                pharmacy1: pharmacies[i],
-                pharmacy2: pharmacies[j],
-                distance: data.features[0].properties.summary.distance,
-                duration: data.features[0].properties.summary.duration
-            });
+        for (let j = 0; j < pharmacies.length; j++) {
+            if (i === j) {
+                distances.push({
+                    pharmacy1: pharmacies[i],
+                    pharmacy2: pharmacies[j],
+                    distance: 0,
+                    duration: 0
+                });
+            } else {
+                // API Call
+                const response = await fetch(
+                    `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${API_KEY}&start=${pharmacies[i].coordinates}&end=${pharmacies[j].coordinates}`
+                );
+                const data = await response.json();
+                console.log(data);
+                distances.push({
+                    pharmacy1: pharmacies[i],
+                    pharmacy2: pharmacies[j],
+                    distance: data.features[0].properties.summary.distance,
+                    duration: data.features[0].properties.summary.duration
+                });
+            }
         }
     }
 
